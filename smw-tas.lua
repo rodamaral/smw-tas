665e4a0ffd0dfca7bf9e515b135668a198cb3be8
Rodrigo A. do Amaral <taspuzzle@gmail.com>
2017-01-11 11:19:43 -0200

    lsnes: allow just one instance of smw-tas running in the Lua VM

--- smw-tas.lua
+++ smw-tas.lua
@@ -2,6 +2,11 @@ local SCRIPTS_FOLDER = "src"
 local ERROR_MESSAGE =  "Your emulator is not supported"
 
 if lsnes_features then -- lsnes emulator
+  -- run just one instance of smw-tas per Lua VM
+  assert(not SMW_TAS_RUNNING, "smw-tas is already running!\n" ..
+    "You can reset all the Lua Virtual Machine at Tools > Reset Lua VM")
+  SMW_TAS_RUNNING = true
+
   local LUA_SCRIPT_FILENAME = load([==[return @@LUA_SCRIPT_FILENAME@@]==])()
 
   GLOBAL_SMW_TAS_PARENT_DIR = LUA_SCRIPT_FILENAME:match("(.+)[/\\][^/\\+]")
